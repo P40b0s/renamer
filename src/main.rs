55153d6326ba46dd55dd8e2560765af9ef80d785
P40b0s <uranius666@gmail.com>
2025-01-28 17:48:45 +0300

    return back

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,78 @@
-mod copy;
-mod settings;
-use std::{path::{PathBuf}, thread, time::Duration};
-extern crate chrono;
-use chrono::Local;
-use settings::Settings;
+use std::{fs, path::PathBuf};
 
 
-const DATE_FORMAT_STR: &'static str = "%Y-%m-%d][%H:%M:%S";
 fn main()
 {
-   if let Some(settings) = Settings::load_settings()
-   {
-        let mut except = copy::get_except_names(&settings);
-        if except.is_some()
-        {
-            let mut except = except.as_mut().unwrap();
-            loop 
-            {
-                run_process(&settings, &mut except);
-                thread::sleep(Duration::from_millis(settings.timer));
-            }
-        }
-        else
-        {
-            println!("Ошибка получения списка фафлов...");
-            std::io::stdin().read_line(&mut String::new()).unwrap();
-        }
-   }
-   else 
-   {
-        println!("Ошибка чтения настроек...");
-        std::io::stdin().read_line(&mut String::new()).unwrap();
-   }
+    rename();
 }
 
-fn run_process(settings: &Settings, except: &mut Vec<String>)
+fn rename() 
 {
-    if let Some(dirs) = copy::get_dirs(settings)
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2
     {
-        for d in dirs
+        println!("⚠️ Ошибка, не передано имя файла с картой переименования");
+        std::process::exit(0x0100);
+    }
+    let file_name = &args[1];
+    let mut source_name : Vec<PathBuf> = vec![];
+    let mut target_name : Vec<PathBuf> = vec![];
+    let fl = fs::read_to_string(file_name);
+    let current_dir = std::env::current_dir().expect("⚠️ Невомзможно определить текущую директорию!");
+    if let Ok(file) = fl
+    {
+       for l in file.lines()
+       {
+        if !l.contains(',')
         {
-            if !except.contains(&d)
-            {
-                except.push(d.clone());
-                let mut target: PathBuf = PathBuf::from(settings.out_dir.as_path());
-                let mut source: PathBuf = PathBuf::from(settings.in_dir.as_path());
-                target.push(d.as_str());
-                source.push(d.as_str());
-                let dt = Local::now();
-                println!("{} Обнаружена директория {}, копирую в {}", dt.format(DATE_FORMAT_STR),  source.display(), target.display());
-                copy::copy_recursively(source.as_path(), target.as_path());
-            }
+            println!("⚠️ Ошибка, значение {} не содержит раделителя - , (зпт)", l);
+            std::process::exit(0x0100);
         }
+            let sp : Vec<&str> = l.split(",").collect();
+            let mut dir_1 = current_dir.clone();
+            let mut dir_2 = current_dir.clone();
+            dir_1.push(sp[0].trim());
+            dir_2.push(sp[1].trim());
+
+            if !dir_1.exists()
+            {
+                if dir_2.exists()
+                {
+                    println!("😳 Директория {} уже имеет конечное наименование из файла сопоставления", dir_2.display());
+                }
+                else
+                {
+                    println!("⚠️ Ошибка, директория {} не существует!", dir_1.display());
+                    std::process::exit(0x0100);
+                }
+            }
+            else
+            {
+                source_name.push(dir_1);
+                target_name.push(dir_2);
+            }
+
+            for (i, dir) in source_name.iter().enumerate()
+            {
+                let target = target_name.iter().nth(i).expect(&format!(" ⚠️Директория соспоставляемая с {} не найдена", dir.display()));
+                if cfg!(windows) 
+                {
+                    if let Err(e) = fs::rename(&dir, target)
+                    {
+                        fs::rename(&dir, target);
+                    }
+                } 
+                else if cfg!(unix) 
+                {
+                    fs::rename(&dir, target).expect(format!("⚠️ Ошибка переименования {} в {}", dir.display(), target_name[i].display()).as_str());
+                }
+                
+                println!("😉 Переименование {} -> {}", dir.display(), target.display());
+            }
+       }
+    }
+    else
+    {
+        println!("⚠️ В директории {} файл переименования {} не найден. ({})", current_dir.display(), file_name, fl.err().unwrap());
     }
 }
